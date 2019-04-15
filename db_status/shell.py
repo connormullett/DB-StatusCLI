2aa16f8b6f6dc6ff28788550a01adf6b38a57714
connormullett <connormullett@gmail.com>
2019-04-15 10:34:26 -0400

    readme changes, in progress templating

--- db_status/shell.py
+++ db_status/shell.py
@@ -15,20 +15,24 @@ def clear(*args, **kwargs):
 
 
 def get_uri(*args, **kwargs):
-    '''
-    returns the URI of the database
-    currently connected to
-    '''
+    '''returns the URI of the database
+    currently connected to'''
     sys.stdout.write(f'{kwargs.get("uri")}\n')
 
 
-def print_help():
+def change_uri(*args, **kwargs):
+    '''change the URI to a different database, will not write to config'''
+    pass
+
+
+def print_help(*args, **kwargs):
     '''print commands with descriptions to terminal'''
     sys.stdout.write(
         f' /h :: {print_help.__doc__}\n' \
         f' /q :: {quit.__doc__}\n' \
         f' /l :: {clear.__doc__}\n' \
         f' /u :: {get_uri.__doc__}\n' \
+        f' /r :: {change_uri.__doc__}\n' \
     )
 
 
@@ -38,7 +42,8 @@ _commands = {
     'h': print_help,
     'q': quit,
     'l': clear,
-    'u': get_uri
+    'u': get_uri,
+    'r': change_uri
 }
 
 def pp(rows):
@@ -75,6 +80,14 @@ def shell(conn: Connection):
             if c[1] in _commands:
                 command = _commands.get(c[1], None)
                 command(uri=uri)
+            if c[1].lower() == 'r':
+                # TODO: perform URI switch here
+                # should perform health check
+                # should change uri in conf file
+                # may need to add bool for if the uri
+                # was supplied through option or
+                # pulled from config file
+                pass
 
         else:
             try:
